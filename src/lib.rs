c40b322f94ab8d1f1f8bc4f6991d74689330742d
blu-dev <the.blu.dev@gmail.com>
2021-09-02 12:49:20 -0700

    basic cargo test

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,7 @@
 mod tree;
+
+#[test]
+fn sample_test() {
+    let mut tree = tree::FsTree::new();
+    tree.add_path("/mnt/c/Users", "coe_a/Downloads").unwrap();
+}
--- src/tree.rs
+++ src/tree.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use std::path::{Path, PathBuf};
 use std::io;
 
+use std::borrow::Borrow;
 use std::cmp::PartialEq;
 use std::ops::{Deref, DerefMut};
 
@@ -19,7 +20,13 @@ pub enum TreeError {
     #[error("The path '{0}' is missing a file name!")]
     NoFileName(PathBuf),
     #[error("The path '{0}' does not contain a valid filename!")]
-    InvalidFileName(PathBuf)
+    InvalidFileName(PathBuf),
+    #[error("Attempting to add child entry to file at '{0}'!")]
+    FileChild(PathBuf),
+    #[error("Path already exists, consumed by '{0}'")]
+    OwnedPath(PathBuf),
+    #[error("Failed to add child '{0}' to node at '{1}' despite it not existing!")]
+    PhantomNode(String, PathBuf),
 }
 
 enum FsEntryType {
@@ -27,11 +34,31 @@ enum FsEntryType {
     File(usize)
 }
 
+impl FsEntryType {
+    pub fn is_dir(&self) -> bool {
+        match self {
+            Self::Directory(_) => true,
+            Self::File(_) => false
+        }
+    }
+
+    pub fn is_file(&self) -> bool {
+        match self {
+            Self::Directory(_) => false,
+            Self::File(_) => true
+        }
+    }
+}
+
 // Separate implementations of a hash are important for keeping track of files in different systems
 trait TreeNode {
     type TreeKey;
+    type ErrorType;
 
     fn get_key(&self) -> Self::TreeKey;
+
+    fn on_child_added(&mut self, child: &Self) -> Result<(), Self::ErrorType>;
+    fn on_child_removed(&mut self, child: Self) -> Result<(), Self::ErrorType>;
 }
 
 struct Node<T: TreeNode> {
@@ -40,9 +67,54 @@ struct Node<T: TreeNode> {
     children: HashMap<T::TreeKey, Node<T>>
 }
 
-impl<T: TreeNode> Node<T> where <T as TreeNode>::TreeKey: Hash + Eq {
-    pub fn get_child<A: AsRef<T::TreeKey>>(&self, key: A) -> Option<&Node<T>> {
-        self.children.get(key.as_ref())
+impl<T: TreeNode> Node<T> where <T as TreeNode>::TreeKey: Hash + Eq + Clone{
+    pub fn new(base: T) -> Self {
+        let key = base.get_key();
+        Self {
+            key,
+            data: base,
+            children: HashMap::new()
+        }
+    }
+
+    pub fn get_child<A: Borrow<T::TreeKey>>(&self, key: A) -> Option<&Self> {
+        self.children.get(key.borrow())
+    }
+
+    pub fn get_child_mut<A: Borrow<T::TreeKey>>(&mut self, key: A) -> Option<&mut Self> {
+        self.children.get_mut(key.borrow())
+    }
+
+    pub fn try_add_child(&mut self, data: T, overwrite: bool) -> &mut Self {
+        let key = data.get_key();
+        if !self.children.contains_key(&key) | overwrite {
+            if let Some(original) = self.children.insert(key.clone(), Self::new(data)) {
+                let Self { key: _, data: data, children: _ } = original;
+                self.data.on_child_removed(data);
+            }
+            let data = self.children.get(&key).expect("Failed to find child immediately after adding it!");
+            let data = &data.data;
+            self.data.on_child_added(data);
+        }
+        self.children.get_mut(&key).expect("Failed to find child after guaranteeing existence!")
+    }
+
+    pub fn add_child(&mut self, data: T, overwrite: bool) -> Result<bool, T::ErrorType> {
+        let key = data.get_key();
+        // TODO: Change from nightly once rust allows try insert
+        if self.children.contains_key(&key) {
+            if overwrite {
+                let Self { key: _, data: data, children: _ } = self.children.insert(key, Self::new(data)).expect("No original member found despite promise!");
+                self.data.on_child_removed(data);
+            }
+            Ok(overwrite)
+        } else {
+            self.children.insert(key.clone(), Self::new(data));
+            let data = self.children.get(&key).expect("Failed to find child immediately after adding it!");
+            let data = &data.data;
+            self.data.on_child_added(data);
+            Ok(true)
+        }
     }
 } 
 
@@ -61,9 +133,26 @@ pub struct FsNode {
 
 impl TreeNode for FsNode {
     type TreeKey = String;
+    type ErrorType = TreeError;
 
     fn get_key(&self) -> Self::TreeKey {
         self.name.clone()
+    }
+
+    fn on_child_added(&mut self, _child: &Self) -> Result<(), Self::ErrorType> {
+        if let FsEntryType::Directory(count) = &mut self.entry_type {
+            *count += 1;
+            Ok(())
+        } else {
+            Err(TreeError::FileChild(self.local_path.clone()))
+        }
+    }
+
+    fn on_child_removed(&mut self, _child: Self) -> Result<(), Self::ErrorType> {
+        if let FsEntryType::Directory(count) = &mut self.entry_type {
+            *count -= 1;
+        }
+        Ok(())
     }
 }
 
@@ -72,12 +161,12 @@ impl FsNode {
         if !path.exists() {
             Err(TreeError::PathDoesNotExist(path.to_path_buf()))
         } else {
-            match std::fs::metadata(&path) { // probably a better way to do this but for now this is fine
+            match std::fs::metadata(&path) {
                 Ok(metadata) => {
                     if metadata.is_file() {
                         Ok(FsEntryType::File(metadata.len() as usize))
                     } else if metadata.is_dir() {
-                        Ok(FsEntryType::Directory(std::fs::read_dir(path).unwrap().count()))
+                        Ok(FsEntryType::Directory(0))
                     } else {
                         Err(TreeError::InvalidMetadata(path.to_path_buf()))
                     }
@@ -99,6 +188,15 @@ impl FsNode {
             None => {
                 Err(TreeError::NoFileName(path.to_path_buf()))
             }
+        }
+    }
+
+    pub(crate) fn empty() -> Self {
+        Self {
+            name: String::default(),
+            local_path: PathBuf::default(),
+            root_path: PathBuf::default(),
+            entry_type: FsEntryType::Directory(0)
         }
     }
 
@@ -143,25 +241,31 @@ impl FsNode {
 }
 
 #[repr(transparent)]
-pub struct FsTree(FsNode);
-
-impl Deref for FsTree {
-    type Target = FsNode;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-
-impl DerefMut for FsTree {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        &mut self.0
-    }
-}
+pub struct FsTree(Node<FsNode>);
 
 impl FsTree {
-    pub fn has_entry() {
+    pub fn add_path<P: AsRef<Path>, R: AsRef<Path>>(&mut self, root_path: P, local_path: R) -> Result<(), TreeError> {
+        let root_path = root_path.as_ref();
+        let local_path = local_path.as_ref();
+        let mut components = local_path.components();
+        let mut current_path = PathBuf::new();
 
+        let mut current_node = Some(&mut self.0);
+        while let Some(node) = current_node.take() {
+            if let Some(next) = components.next() {
+                if node.data.entry_type.is_file() {
+                    return Err(TreeError::FileChild(root_path.join(&current_path)));
+                }
+                let next = next.as_os_str();
+                current_path = current_path.join(next);
+                current_node = Some(node.try_add_child(FsNode::new(&current_path, root_path)?, false));
+            }
+        }
+
+        Ok(())
+    }
+
+    pub fn new() -> Self {
+        Self(Node::new(FsNode::empty()))
     }
 }
